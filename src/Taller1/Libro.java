2a5ba5bfce26ef9f0d49053c62fcff493f4d17ee
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 13:13:01 -0500

    Creación de las propiedades de la clase y asimismo los constructores, uno por defecto y otro parametrizado

--- src/Taller1/Libro.java
+++ src/Taller1/Libro.java
@@ -1,4 +1,21 @@
 package Taller1;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private int nrmPaginas;
+
+    //CONSTRUCTORES
+
+    public Libro(){
+        titulo="Sin titulo";
+        autor="Sin autor";
+        nrmPaginas=0;
+    }
+    public Libro(String titulo, String autor, int nrmPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.nrmPaginas = nrmPaginas;
+    }
+
 }
