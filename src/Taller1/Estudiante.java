f6b6fd67c6dee8ee2442597762ead4281b276636
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 13:59:31 -0500

    Creación de los constructores, uno por defecto, otro parametrizado con 2 parametros y uno ultimo sobrecargado y que hace empleo del this() para llamar al constructor parametrizado dentro del constructor sobrecargado, todo esto para la clase Estudiante

--- src/Taller1/Estudiante.java
+++ src/Taller1/Estudiante.java
@@ -5,5 +5,19 @@ public class Estudiante {
     private int edad;
     private String curso;
 
+    // CONSTRUCTORES
 
+    public Estudiante(){
+        nombre = "Sin nombre";
+        edad = 0;
+        curso = "Sin curso";
+    }
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    public Estudiante(String nombre, int edad, String curso) {
+        this(nombre, edad);
+        this.curso = curso;
+    }
 }
