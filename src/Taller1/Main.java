87ac4085a6c73d15bcc3209c90697d2dea5ed8f0
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 16:37:44 -0500

    Creacion de la clase main y posterior a esto, se crearon 2 objetos por cada clase usando el constructor por defecto de cada clase,y usando en cada una o el constructor sobre cargado o el el constructor parametrizado

---
+++ src/Taller1/Main.java
@@ -0,0 +1,22 @@
+package Taller1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Libros
+        Libro libro0 = new Libro();
+        Libro libro1 = new Libro("El Principito", "Antoine de Saint-Exupéry", 96);
+
+        // Cuentas Bancarias
+        CuentaBancaria cuenta0 = new CuentaBancaria();
+        CuentaBancaria cuenta1 = new CuentaBancaria();
+
+        // Estudiantes
+        Estudiante estudiante0 = new Estudiante();
+        Estudiante estudiante1 = new Estudiante( "Maria Camila", 20);
+
+
+
+
+    }
+}
