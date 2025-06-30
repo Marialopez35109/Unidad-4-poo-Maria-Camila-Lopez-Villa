b91ce8b86d738a286ebb64d050b3b716426b4870
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 14:01:39 -0500

    Creación de los metodos y funciones GETTERS Y SETTERS

--- src/Taller1/Estudiante.java
+++ src/Taller1/Estudiante.java
@@ -20,4 +20,24 @@ public class Estudiante {
         this(nombre, edad);
         this.curso = curso;
     }
+
+    // GETTERS Y SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
 }
