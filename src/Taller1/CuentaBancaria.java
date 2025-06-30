804215d5710c09fdd94423ff11844d24d0e245c7
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 13:37:15 -0500

    Creaciones de constructores, uno por defecto, el cual tiene valores predeterminados, otro paramatrizado pero con 2 parametros, y uno ultimo sobrecargado el cual contiene 3 parametros, todo esto para la clase CuentaBancaria

--- src/Taller1/CuentaBancaria.java
+++ src/Taller1/CuentaBancaria.java
@@ -5,4 +5,19 @@ public class CuentaBancaria {
     private double saldo;
     private String tipocuenta;
 
+    // CONSTRUCTORES
+    public CuentaBancaria(){
+        nrmCuenta = "Nulo";
+        saldo = 0.0;
+        tipocuenta = "Nulo";
+    }
+    public CuentaBancaria(String nrmCuenta,String tipocuenta){
+        this.nrmCuenta = nrmCuenta;
+        this.tipocuenta = tipocuenta;
+    }
+    public CuentaBancaria(String nrmCuenta,double saldo, String tipocuenta){
+        this.nrmCuenta = nrmCuenta;
+        this.saldo = saldo;
+        this.tipocuenta = tipocuenta;
+    }
 }
