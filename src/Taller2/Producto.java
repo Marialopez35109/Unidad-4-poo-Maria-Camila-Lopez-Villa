e4e026ba8333f1bfc01e1f83f5a706880f65792e
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 20:49:59 -0500

    Creación de los metodos y funciones GETTERES Y SETTERS, y asimismo del metodo mostrarDetallesProducto

--- src/Taller2/Producto.java
+++ src/Taller2/Producto.java
@@ -10,4 +10,23 @@ public class Producto {
     this.precio=precio;
     }
 
+    // GETTERS Y SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getPrecio() {
+        return precio;
+    }
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    // METODO PARA MOSTRAR DETALLES DEL PRODUCTO
+    public void mostrarDetallesProducto() {
+        System.out.println("Nombre del Producto: " + getNombre());
+        System.out.println("Precio del Producto: " + getPrecio());
+    }
 }
