64bdaaaf92c1e2c56500b7f127617fbe41657ebe
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 22:44:11 -0500

    Creación de constructor que tiene la funcion de incrementar contadorCoches cada que se cree un objeto de la clase, asimismo se crearon los metodos y funciones GETTERS Y SETTERS

--- src/Taller3/Coche.java
+++ src/Taller3/Coche.java
@@ -5,4 +5,29 @@ public class Coche {
     private String modelo;
     private static int contadorCoches=0;
 
+    // Constructor
+    public Coche (){
+        contadorCoches++;
+    }
+
+    // Getters y Setters
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public static int getContadorCoches() {
+        return contadorCoches;
+    }
+    public static void setContadorCoches(int contadorCoches) {
+        Coche.contadorCoches = contadorCoches;
+    }
+
 }
