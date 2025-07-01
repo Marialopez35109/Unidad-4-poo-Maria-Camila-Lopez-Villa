62e0b5ef82d7f83b82ce2ebcd35ff739d143d0c4
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-01 13:25:16 -0500

    Creación de los metodos y funciones GETTERS Y SETTERS, a estos ultimo se les implementó validaciones, como para que el espacio no este vacio o nulo, para un rango de edad y para un rango de nota

--- src/Taller4/Estudiante.java
+++ src/Taller4/Estudiante.java
@@ -2,8 +2,40 @@ package Taller4;
 
 public class Estudiante {
     private String nombre;
-    private String edad;
+    private int edad;
     private double notaPromedio;
 
+    // Setters y getters
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        if(nombre== null || nombre.isEmpty()) {
+            System.out.println("Nombre no puede ser nulo o vacío");
+            return;
+        }else {
+            this.nombre = nombre;
+        }
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        if (edad>= 0 && edad <= 120){
+            this.edad = edad;
+        }else{
+            System.out.println("Edad no valida");
+        }
+    }
+    public double getNotaPromedio() {
+        return notaPromedio;
+    }
+    public void setNotaPromedio(double notaPromedio) {
+        if (notaPromedio >= 0.0 && notaPromedio <= 5.0) {
+            this.notaPromedio = notaPromedio;
+        } else {
+            System.out.println("La nota promedio debe estar entre 0.0 y 5.0.");
+        }
+    }
 
 }
