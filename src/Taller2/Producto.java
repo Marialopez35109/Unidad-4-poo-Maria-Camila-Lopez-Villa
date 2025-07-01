f372bac6fa9c165244790a62cf65cc907ae9a1e9
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 20:43:04 -0500

    Creación del constructor con 2 parametros de la clase Producto

--- src/Taller2/Producto.java
+++ src/Taller2/Producto.java
@@ -4,4 +4,10 @@ public class Producto {
     private String nombre;
     private int precio;
 
+    // CONSTRUCTOR
+    public Producto(String nombre, int precio) {
+    this.nombre=nombre;
+    this.precio=precio;
+    }
+
 }
