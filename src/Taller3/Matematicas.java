40903239e4b4f14e04b14ac6cb9b5475fa42f88b
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 22:59:57 -0500

    Creación de la clase Matematicas, en la cual se crearon los metodos de las operaciones matematicas aritmeticas basicas, un metodo de suma, de resta, multiplicación y de división, en este ultimo se tuvo en cuenta una de las propiedades de la división

---
+++ src/Taller3/Matematicas.java
@@ -0,0 +1,33 @@
+package Taller3;
+
+public class Matematicas {
+
+    public static void suma(){
+        int a = 5;
+        int b = 10;
+        int resultado = a+b;
+        System.out.println("La suma entre " + a + " y " + b + " es: " + resultado);
+    }
+    public static void resta(){
+        int a =20;
+        int b= 15;
+        int resultado = a-b;
+        System.out.println("La resta entre "+a + " y " + b + " es: " + resultado);
+    }
+    public static void multiplicacion(){
+        int a= 30;
+        int b= 90;
+        int resultado= a*b;
+        System.out.println("La multiplicación entre " +a + "por " + b + "es: " + resultado);
+    }
+    public static void division(){
+        int a = 100;
+        int b = 5;
+        if (b != 0) {
+            int resultado = a / b;
+            System.out.println("La división entre " + a + " y " + b + " es: " + resultado);
+        } else {
+            System.out.println("Error: División por cero no permitida.");
+        }
+    }
+}
