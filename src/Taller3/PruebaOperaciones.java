051c9b26446ded884cec2a131706408c1337604e
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-06-30 23:04:50 -0500

    Creación de la clase PruebaOpereaciones, para dar uso de los metodos creados en la clase Matematicas

---
+++ src/Taller3/PruebaOperaciones.java
@@ -0,0 +1,13 @@
+package Taller3;
+
+public class PruebaOperaciones {
+    public static void main(String[] args) {
+
+        // Prueba de las operaciones matemáticas
+        Matematicas.suma();
+        Matematicas.resta();
+        Matematicas.multiplicacion();
+        Matematicas.division();
+
+    }
+}
