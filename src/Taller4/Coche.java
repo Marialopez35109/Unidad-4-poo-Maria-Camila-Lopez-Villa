ee0facf703fa3e1343fbc463562437a7302a858e
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-01 13:56:03 -0500

    Creación de la clase PurebaCoche, que tiene como finalidad probar los errores de compilación que ocurren cuando se intenta acceder directamente a propiedades que son privadas, en este caso de la clase Coche. En esta clase se hizo una instancia de la clase coche, posterior a esto se creara un constructor en la clase Coche, para poder realizar el ejercicio

--- src/Taller4/Coche.java
+++ src/Taller4/Coche.java
@@ -5,10 +5,6 @@ public class Coche {
     private String modelo;
     private double velocidadMaxima=0;
 
-    public Coche(String marca) {
-        this.marca = marca;
-
-    }
     public void acelerar(double incremento) {
         if (incremento>0){
             velocidadMaxima += incremento;
--- src/Taller4/PruebaCoche.java
+++ src/Taller4/PruebaCoche.java
@@ -4,6 +4,5 @@ public class PruebaCoche {
     public static void main(String[]args){
         Coche coche1 =new Coche();
 
-        coche1.modelo;
     }
 }
