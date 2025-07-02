9a7f0797c61aeb117807dc05d5ebd8f477d06299
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-02 00:25:57 -0500

    Creación de la clase PruebaProducto, la cual tiene como finalidad, verificar si efectivamente se puede acceder a los miembros de la clase Producto

---
+++ src/Taller5/PruebaProducto.java
@@ -0,0 +1,9 @@
+package Taller5;
+
+public class PruebaProducto {
+    public static void main(String[] args) {
+        Producto producto1=new Producto("Laptop", 1500.00, 10);
+        producto1.mostrarInfo();
+    }
+
+}
