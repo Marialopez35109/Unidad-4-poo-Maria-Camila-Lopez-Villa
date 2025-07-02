890943e20cf263a53dd270ed5a2b477d2885a5b8
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-02 01:01:26 -0500

    Creación de las funciones y metodos GETTERS Y SETTERS para el atributo nombre

--- src/Taller5/Persona.java
+++ src/Taller5/Persona.java
@@ -4,4 +4,11 @@ public class Persona {
     private String nombre;
     int edad;
 
+    //getter y setter
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
