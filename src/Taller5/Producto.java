5c7238e02aa05e052c22d0745f200071dd5e9b9e
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-02 00:12:32 -0500

    Creación del constructor con tipo de acceso default, en el cual se inicializaron todos los atributos, en esta misma linea, tambien se creo un metodo (mostrarInfo) que tiene como finalidad mostrar las información del producto

--- src/Taller5/Producto.java
+++ src/Taller5/Producto.java
@@ -6,4 +6,15 @@ public class Producto {
     int stock;
 
     // Constructor
+    Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    // Metodo
+    void mostrarInfo(){
+        System.out.println("Nombre: " +nombre);
+        System.out.println("Precio: " + precio);
+        System.out.println("Stock" + stock);
+    }
 }
