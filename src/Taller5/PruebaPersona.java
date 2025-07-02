f3af0e7ac82158c6f58ae6303d4513bb5508c896
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-02 01:24:06 -0500

    Verificación de accesibilidad de metodos y atributos, se verificó, que para poder acceder al atributo que se clasificó como private, es necesario usar los metodos y funciones set y get, set para poder modificar la información y get para obtener dicha información, por otro lado el atributo que se clasificó como default, no requiere de metodos o funciones auxiliares que ayuden a acceder a dicho atributo, es decir, se puede acceder directamente a él, sin ayudas externas, de esto se concluye, que no es posible el acceso a atributos o a miembros private desde una clase externa sin el uso de metodos y funciones get y set, asimismo, lo miembros default no necesitan hacer empleo de estas funciones y metodos para poder ser usados por clases externas que se encuentren en el mismo paquete

--- src/Taller5/PruebaPersona.java
+++ src/Taller5/PruebaPersona.java
@@ -2,6 +2,11 @@ package Taller5;
 
 public class PruebaPersona {
     public static void main(String[] args) {
+    Persona persona = new Persona();
+        persona.setNombre("Juan Perez");
+        persona.edad = 30;
 
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Edad: " + persona.edad);
     }
 }
