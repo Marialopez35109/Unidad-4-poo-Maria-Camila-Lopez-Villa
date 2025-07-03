7d5f0ff845aaf779533eb7033859cdb854492dba
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-02 22:44:44 -0500

    Creación del metodo mostrarInformacion en la clase padre, que tiene como finalidad mostrar la información del empleado, y ademas, la realización del ejercicio, que busca que la clase Gerente sobre escriba dicho metodo

--- src/Taller6/Empleado.java
+++ src/Taller6/Empleado.java
@@ -4,4 +4,8 @@ public class Empleado {
     protected String nombre;
     protected double salario;
 
+    protected void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Salario: " + salario);
+    }
 }
