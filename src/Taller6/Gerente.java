dab6773cab85c778e162d3b756830bdba93039a6
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-02 22:54:20 -0500

    Se sobreescribió el metodo mostrarInformacion de la clase padre, para mostrar la información del empleado y del gerente

--- src/Taller6/Gerente.java
+++ src/Taller6/Gerente.java
@@ -3,5 +3,10 @@ package Taller6;
 public class Gerente extends Empleado {
     protected String departamento;
 
+    @Override
+    protected void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Departamento: " + departamento);
+    }
 
 }
