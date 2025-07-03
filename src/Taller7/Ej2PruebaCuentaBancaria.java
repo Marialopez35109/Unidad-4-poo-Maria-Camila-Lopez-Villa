837bfba3cc26895b839021163dc24e15a83894ba
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-03 13:12:21 -0500

    Creación de la clase Ej2PruebaCuentaBancaria, asimismo la instancia de la clase Ej2CuentaBancaria, e intento de acceder al atributo private nrmCuenta, sin emabrgo hubo un error de compilación, a raiz de que, el atributo es private,y como ya se ha expuesto, la unica forma de acceder a atributos private desde una clase externa es con los metodos set y get

--- src/Taller7/Ej2PruebaCuentaBancaria.java
+++ src/Taller7/Ej2PruebaCuentaBancaria.java
@@ -1,4 +1,10 @@
 package Taller7;
 
 public class Ej2PruebaCuentaBancaria {
+    public static void main(String[] args) {
+        Ej2CuentaBanciaria cuenta = new Ej2CuentaBanciaria();
+        cuenta.nrmCuenta = "123456789";
+        cuenta.mostarDetallesCuenta();
+
+    }
 }
