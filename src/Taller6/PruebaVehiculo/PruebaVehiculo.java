79fd37a4f2e2e7dfee13a1303ddc438245b05042
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-03 12:09:34 -0500

    Creación de la clase Banco, con un atributo protected para saldo, sin embargo esta implementación no es adecuada, al ser un dato tan sencible, ya que, al estar declarado como protected, le da acceso a otras clases y subclases lo que genera una puerta abierta inecesaria, lo mejor seria implementar el modificador de acceso private, ya que encampsula mejor el dato, dandole solo acceso a la clase en la que se encuentra

--- src/Taller6/PruebaVehiculo/PruebaVehiculo.java
+++ src/Taller6/PruebaVehiculo/PruebaVehiculo.java
@@ -4,10 +4,12 @@ import Taller6.Moto;
 import Taller6.Vehiculo;
 
 public class PruebaVehiculo {
+    /*
     public static void main (String[]args){
         Vehiculo vehiculo = new Vehiculo();
         Moto moto = new Moto();
         vehiculo.tipo = "Sedán";
         moto.tipo = "Deportiva";
     }
+    */
 }
