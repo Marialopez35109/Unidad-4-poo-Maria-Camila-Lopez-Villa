d158db8bb72f6c63120a2d675b8d200bcdc92639
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-04 14:08:30 -0500

    Creación de la clase Ej3Base y su atributo,luego se creo la clase Ej3Prueba en la que se creo un metodo que intenta mostrar la edad, sin embargo arroja un error ya que la clase Ej3Prueba no está vinculada o no es hija de la clase Ej3Base, y super solo es valido o solo funciona en subclases, puesto que se refiere a la clase padre

---
+++ src/Taller9/Ej3Base.java
@@ -0,0 +1,6 @@
+package Taller9;
+
+public class Ej3Base {
+    public int edad = 10;
+
+}
---
+++ src/Taller9/Ej3Prueba.java
@@ -0,0 +1,7 @@
+package Taller9;
+
+public class Ej3Prueba {
+    public void mostrar() {
+        System.out.println(super.edad);
+    }
+}
